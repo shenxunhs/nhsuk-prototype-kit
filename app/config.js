a015373537500bc2abfe8640745cd3520f10e528
shenxu83 <shen.xu@nhs.net>
2025-01-29 20:31:54 +0000

    update name of service
    get initial test

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Model Ingress Provenance Checklist',
 
   // Port to run nodemon on locally
   port: 2000,
